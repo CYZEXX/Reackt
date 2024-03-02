8e19a82530a63877668ad903f8814a6f40405f96
CYZEXX <shohruh8851@gmail.com>
2024-03-02 19:39:14 +0500

    React css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react'
+import Title from "./components/Title"
 
 function App() {
   const [name, setName] = useState('John')
@@ -23,11 +24,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      
-      <button onClick={handleClick}>Change name</button>
-      <hr />
-      <br />
+      <Title/>
       {showContent && <button onClick={() => setShowContent(false)}>toggle</button>}
       {!showContent &&  <button onClick={() => setShowContent(true)}>toggle</button>}
       {showContent && events.map((event) => {
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+const Title = () => {
+    return (
+        <div>
+            <h1 className="title">John Kingdom Event</h1>
+            <br />
+            <p className="subtitle">All events wel be here</p>
+        </div>
+    )
+}
+
+export default Title
